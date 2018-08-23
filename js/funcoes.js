b23cdb68a76d8b374a82c525dd1864c035eb9f9f
guilherme63 <guilherme.lima.dinizt@gmail.com>
2018-08-23 20:43:50 -0300

    continuacao java script

---
+++ js/funcoes.js
@@ -0,0 +1,11 @@
+function trocaImagem() {
+	document.getElementById("myImage").src="img/aberta.jpg"; 				
+}
+function imagemTroca() {
+	document.getElementById("myImage").src="img/fechada.jpg";
+}	
+
+function trocaFonte() {
+	document.getElementById("demo").style.fontSize="50px";
+}	
+
